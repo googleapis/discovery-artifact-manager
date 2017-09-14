ba56c945be10d896887e08f09a32329562bd0ce1
Sai Cheemalapati <saicheems@google.com>
2017-09-14 10:57:16 -0700

    Address PR feedback

--- server/tasks/_commit_message.py
+++ server/tasks/_commit_message.py
@@ -1,9 +1,22 @@
-"""Contains a function which builds a nice commit message."""
+# Copyright 2017, Google Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Contains a function which builds a detailed commit message."""
 from datetime import date
 
 
 def build(added, deleted, updated, subject=None):
-    """Returns a nice commit message.
+    """Returns a detailed commit message.
 
     Args:
         added (set): a set of API IDs that have been added.
